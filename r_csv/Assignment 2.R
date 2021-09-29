9e819641b13fcb263806aa97dec9962a33031299
Victor Min <victorshwe@yahoo.com.sg>
2021-09-29 16:23:01 +0800

    Assignment

---
+++ r_csv/Assignment 2.R
@@ -0,0 +1,9 @@
+CSV_wiki <- "https://en.wikipedia.org/wiki/Comma-separated_values"
+CSV_html <- read_html(CSV_wiki)
+
+table <- CSV_html %>%
+  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[2]') %>%
+  html_table()
+
+write.csv(table, "Assignment 2.csv")
+
