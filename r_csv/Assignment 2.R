6dec0bdc9be6e9fb12f4267b71c7c91a10d42364
Victor Min <90329393+Biscuitkru@users.noreply.github.com>
2021-10-14 15:54:22 +0800

    Update Assignment 2.R

--- r_csv/Assignment 2.R
+++ r_csv/Assignment 2.R
@@ -1,9 +1,12 @@
+library(rvest)
+
 CSV_wiki <- "https://en.wikipedia.org/wiki/Comma-separated_values"
 CSV_html <- read_html(CSV_wiki)
 
 table <- CSV_html %>%
   html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[2]') %>%
   html_table()
+table
 
 write.csv(table, "Assignment 2.csv")
 
